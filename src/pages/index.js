43703ff28172a3aec277cd31540fe6d95a19ffdf
davidc94 <davidc94@btinternet.com>
2019-02-27 19:35:50 +0000

    created general structure of index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import { createGlobalStyle } from 'styled-components';
 import StyledHeader from '../components/atoms/header';
 import StyledText from '../components/atoms/text';
+import StyledDiv from '../components/atoms/div';
+import StyledListItem from '../components/atoms/list';
 import StyledListContainer from '../components/molecules/listcontainer';
 import Colors from '../utils/colors';
 
@@ -12,17 +14,31 @@ const GlobalStyle = createGlobalStyle`
 `
 
 export default () => (    
-    <div>
-        <GlobalStyle />
-        <StyledHeader size='large'>
-            Hello!
-        </StyledHeader>    
-        <StyledHeader size='base'>
-            Welcome to my website!
-        </StyledHeader>
-        <StyledText size='large'>
-            My name is David
-        </StyledText>
-        <StyledListContainer />
-    </div>
+  <div>
+    <GlobalStyle />
+    <StyledHeader size='large'>
+      David Chellapah
+    </StyledHeader>    
+    <StyledHeader size='base'>
+      My name is David and welcome to my website 😎 I am a software developer from London (born and raised).
+    </StyledHeader>
+    <StyledText size='large'>
+      Projects
+    </StyledText>
+    <StyledText size='base'>
+      Here's a list of stuff that i've done in my life that I'm pretty proud of 👏
+    </StyledText>
+    <StyledDiv borderWith='small' borderColor='border' >
+      <StyledListItem size='base'>
+        This website
+      </StyledListItem>
+      <StyledListItem size='base'>
+        Smart Home App (Uni dissertation)
+      </StyledListItem>
+    </StyledDiv>
+    <StyledText>
+      Now that the boring stuff is out of the way, I also enjoy photography and shooting videos so here's a 
+      small collection of of videos and pictures from travelling ✈️.
+    </StyledText>
+  </div>
 )
