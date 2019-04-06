6c0a635f6be00836611bc40971c33688287fd031
davidc94 <davidc94@btinternet.com>
2019-04-06 14:00:41 +0100

    added typing effect to subheader and removed from main header

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -6,19 +6,32 @@ import StyledDiv from '../../atoms/div';
 
 const Header = (props) => (
   <StyledDiv backgroundColor={Colors.blue} width='full'>
-    <Typing>
-      <StyledHeader size='huge' color={Colors.lightGrey}>  
-        <Typing.Speed ms={75} />
+    <StyledDiv backgroundColor={Colors.blue}>  
+      <StyledHeader size='huge' color={Colors.lightGrey}>
         David
       </StyledHeader>
-      <Typing.Delay ms={1000} />
       <StyledHeader size='large' color={Colors.lightGrey}>
-        <Typing.Speed ms={75} />
         Chellapah
       </StyledHeader>
-    </Typing>
+    </StyledDiv>
+
     <StyledHeader size='base' color={Colors.lightGrey}>
-      Hello and welcome to my website 😎 I am a software developer from London (born and raised).
+      <Typing loop>
+        <span>
+          Hello and welcome to my website
+        </span>
+        <Typing.Delay ms={1000} />
+        <Typing.Speed ms={10} />
+        <Typing.Backspace count={37} />
+        <Typing.Speed ms={50} />
+        <span>
+          I am a software developer from London
+        </span>
+        <Typing.Delay ms={1000} />
+        <Typing.Speed ms={10} />
+        <Typing.Backspace count={37} />
+        <Typing.Speed ms={50} />
+      </Typing>
     </StyledHeader>
   </StyledDiv>
 );
