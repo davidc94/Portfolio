df97b823a27b8558b38045d9d8ee9403155bd716
davidc94 <davidc94@btinternet.com>
2019-04-06 10:52:34 +0100

    added typing effect to header

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typing from 'react-typing-animation';
 import Colors from '../../../utils/colors';
 import StyledHeader from '../../atoms/header';
 import StyledDiv from '../../atoms/div';
@@ -6,10 +7,13 @@ import StyledDiv from '../../atoms/div';
 const Header = (props) => (
   <StyledDiv backgroundColor={Colors.blue} width='full'>
     <StyledHeader size='large' color={Colors.lightGrey}>
-      David Chellapah
-    </StyledHeader>    
+      <Typing>
+        <Typing.Speed ms={75} />
+        David Chellapah
+      </Typing>
+    </StyledHeader>
     <StyledHeader size='base' color={Colors.lightGrey}>
-      My name is David and welcome to my website 😎 I am a software developer from London (born and raised).
+      Hello and welcome to my website 😎 I am a software developer from London (born and raised).
     </StyledHeader>
   </StyledDiv>
 );
