020408faeeb84273284dd6c1a1eb4f8b4ab96a66
davidc94 <davidc94@btinternet.com>
2019-04-09 19:47:05 +0100

    flexin on the header

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -5,8 +5,8 @@ import StyledHeader from '../../atoms/header';
 import StyledDiv from '../../atoms/div';
 
 const Header = (props) => (
-  <StyledDiv backgroundColor={Colors.blue} width='full'>
-    <StyledDiv backgroundColor={Colors.blue}>  
+  <StyledDiv backgroundColor={Colors.blue} width='full' style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }} >
+    <StyledDiv backgroundColor={Colors.blue} style={{ flex: 1, paddingLeft: '16px' }} >
       <StyledHeader size='huge' color={Colors.lightGrey}>
         David
       </StyledHeader>
@@ -15,25 +15,29 @@ const Header = (props) => (
       </StyledHeader>
     </StyledDiv>
 
-    <StyledHeader size='base' color={Colors.lightGrey}>
-      <Typing loop>
-        <span>
-          Hello and welcome to my website
-        </span>
-        <Typing.Delay ms={1000} />
-        <Typing.Speed ms={10} />
-        <Typing.Backspace count={37} />
-        <Typing.Speed ms={50} />
-        <span>
-          I am a software developer from London
-        </span>
-        <Typing.Delay ms={1000} />
-        <Typing.Speed ms={10} />
-        <Typing.Backspace count={37} />
-        <Typing.Speed ms={50} />
-        <Typing.Delay ms={1000} />
-      </Typing>
-    </StyledHeader>
+    <StyledDiv backgroundColor={Colors.blue} style={{ flex: 1 }} >
+      <StyledDiv backgroundColor={Colors.blue} >
+        <StyledHeader size='base' color={Colors.lightGrey}>
+          <Typing loop>
+            <span>
+              Hello and welcome to my website
+            </span>
+            <Typing.Delay ms={1000} />
+            <Typing.Speed ms={10} />
+            <Typing.Backspace count={37} />
+            <Typing.Speed ms={50} />
+            <span>
+              I am a software developer from London
+            </span>
+            <Typing.Delay ms={1000} />
+            <Typing.Speed ms={10} />
+            <Typing.Backspace count={37} />
+            <Typing.Speed ms={50} />
+            <Typing.Delay ms={1000} />
+          </Typing>
+        </StyledHeader>
+      </StyledDiv>
+    </StyledDiv>
   </StyledDiv>
 );
 
